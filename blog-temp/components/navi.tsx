d1fca23dfef32bc66a2dd31f629144b477c1fe59
Adrian Bruno <abruno.heel@gmail.com>
2023-09-24 17:21:25 -0400

    updated navigation dropdown

--- blog-temp/components/navi.tsx
+++ blog-temp/components/navi.tsx
@@ -1,12 +1,20 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
+  const [mounted, setMountState] = useState(false);
   const [showList, setListState] = useState(false);
+
+  // useEffect(() => {
+  //   setMountState(true);
+  // }, []);
+  // if (!mounted) return <></>;
+
   return (
-    <div className="text-white">
-      <button className="text-white" onClick={() => setListState(!showList)}>
-        Click Me!
+    <div className="text-[#006600]">
+      <button className="hover:bg-red-400" onClick={() => setListState(!showList)}>
+        Menu
       </button>
       {showList ? <NavList /> : null}
     </div>
@@ -15,13 +23,18 @@ const Navbar = () => {
 
 const NavList = () => {
   return (
-    <>
+    <div className="absolute">
       <ul>
-        <li>Thing1</li>
-        <li>Thing2</li>
-        <li>ETC</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/MusicReviews">Music Reviews</Link>
+        </li>
+        <li>Art</li>
+        <li>Writing</li>
       </ul>
-    </>
+    </div>
   );
 };
 
