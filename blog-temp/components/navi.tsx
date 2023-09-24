e290507ad3ea331919e06999f2feb68c62f54a80
dannyo1 <dannyo1@live.unc.edu>
2023-09-24 15:29:34 -0400

    Hey Adrian

---
+++ blog-temp/components/navi.tsx
@@ -0,0 +1,17 @@
+let navbar = () => {
+
+<main>
+<div className="dropdown">
+<button className="dropbtn">Dropdown 
+      <i className="fa fa-caret-down"></i>
+    </button>
+    <div className="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+</div>
+
+
+</main>
+}
